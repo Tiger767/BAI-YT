a0d7e256a670d2091602df8eacbed722302056b4
unknown <47340315+Tiger767@users.noreply.github.com>
2019-12-17 14:01:22 -0800

    Added more video 5 resources

---
+++ video5/code_from_video.py
@@ -0,0 +1,28 @@
+from image import *
+
+x1 = load('corn_field.jpg', (512, 512))
+x2 = load('sunset.jpg', (512, 512))
+
+ws = Windows()
+ws.start() # make sure to call ws.stop() 
+cf = ws.add('corn_field', x1)
+ss = ws.add('sunset', x2)
+
+ws.set(cf, gray(x1))
+ws.set(ss, resize(x2, (64, 64)))
+ws.set(cf, pyr(x1, -1))
+save('modified_corn_field.jpg', increase_brightness(x1, 50, relative=True))
+ws.set(cf, set_brightness(x1, 10))
+ws.set(cf, set_gamma(x1, 2.0))
+ws.set(ss, gray(x2))
+ws.set(ss, apply_clahe(gray(x2)))
+ws.set(ss, apply_clahe(x2))
+ws.set(ss, equalize(gray(x2)))
+ws.set(cf, rotate(x1, 45))
+ws.set(cf, translate(x1, 50, 100))
+ws.set(ss, crop_rect(x2, 10, 20, 40, 80))
+ws.set(ss, shrink_sides(x2, 20, 20, 20, 20))
+ws.set(ss, pad(x2, 20, 20, 20, 20))
+img = ws.add(image=blend(x1, x2, .7))
+ws.set(cf, zoom(x1, (64, 64), 0, 0))
+ws.stop()
